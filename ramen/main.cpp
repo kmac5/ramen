edba54d9eae3ee92c7da6cbfc4d3df7db10b210e
est77 <ramenhdr@gmail.com>
2013-07-19 01:37:27 +0200

    Minor changes.

--- ramen/main.cpp
+++ ramen/main.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 Esteban Tovagliari
+// Copyright (c) 2013 Esteban Tovagliari
 // Licensed under the terms of the CDDL License.
 // See CDDL_LICENSE.txt for a copy of the license.
 
@@ -17,9 +17,9 @@
 
 void ramen_terminate( void)
 {
-	ramen::app().fatal_error( "Ramen has encountered a fatal error");
+    ramen::app().fatal_error( "Ramen has encountered a fatal error");
 }
-	
+
 void ramen_unexpected( void)
 {
     ramen::app().fatal_error( "Ramen has encountered an unexpected exception");
@@ -27,26 +27,26 @@ void ramen_unexpected( void)
 
 int main( int argc, char **argv)
 {
-	std::set_terminate( &ramen_terminate);
-	std::set_unexpected( &ramen_unexpected);
-	
-	try
-	{
+    std::set_terminate( &ramen_terminate);
+    std::set_unexpected( &ramen_unexpected);
+
+    try
+    {
         ramen::application_t r_app( argc, argv);
-		int result = r_app.run();
-		return result;
-	}
+        int result = r_app.run();
+        return result;
+    }
     catch( ramen::core::exception& e)
     {
         std::cerr << "Exception: " << e.what() << "\n";
     }
-	catch( std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << "\n";
+    catch( std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
-	catch( ...)
-	{
-		std::cerr << "Unknown exception" << std::endl;
+    catch( ...)
+    {
+        std::cerr << "Unknown exception" << std::endl;
     }
 
     return EXIT_FAILURE;
